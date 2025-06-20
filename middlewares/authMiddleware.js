f09e14faf3e59750a2c18f0148e689103b400c68
phantom101-IB <mi95920@gmail.com>
2025-06-20 21:17:28 +0000

    idk 2

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,24 +2,29 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel.js");
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({ msg: "Login fisrt" });
-  }
-  const payload = jwt.verify(token, process.env.JWT_SECRET);
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({ msg: "Login fisrt" });
+    }
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
 
-  if (!payload) {
-    return res.status(401).json({ msg: "Session Expired" });
-  }
-  const userId = payload.userId;
-  const user = await User.findById({ _id: userId }).select("-password");
+    if (!payload) {
+      return res.status(401).json({ msg: "Session Expired" });
+    }
+    const userId = payload.userId;
+    const user = await User.findById({ _id: userId }).select("-password");
 
-  if (!user || user.length === 0) {
-    return res.status(401).json({ msg: "User not found" });
-  }
+    if (!user || user.length === 0) {
+      return res.status(401).json({ msg: "User not found" });
+    }
 
-  req.user = user;
-  next();
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 module.exports = authMiddleware;
